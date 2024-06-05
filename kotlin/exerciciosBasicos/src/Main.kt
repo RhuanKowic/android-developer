9e0ee1230c941592d5f87732dd6e7b26a8515f71
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-05 11:05:17 -0300

    Script para criar arquivos kotlin vazio.

--- kotlin/exerciciosBasicos/src/Main.kt
+++ kotlin/exerciciosBasicos/src/Main.kt
@@ -1,3 +1,27 @@
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val quantArchive: Int = 28
+    val content = ""
+    val repository = "src/EstruturaDeDecisao"
+    for (i in 1..quantArchive){
+        var nameArchive = "Exercicio"
+
+        if( i < 10) nameArchive = "Exercicio0${i}.kt"
+        else nameArchive = "Exercicio${i}.kt"
+
+        createFileKotlin(repository, nameArchive, content)
+    }
+}
+
+fun createFileKotlin(repository: String,nameArchive: String, content: String){
+    val diretoryFile = File(repository)
+    val archive = File(diretoryFile, nameArchive)
+    try {
+        diretoryFile.mkdirs()
+        archive.writeText(content)
+        println("Arquivo $nameArchive criado com sucesso")
+    } catch (ex: Exception){
+        println("Ocorreu um erro ao criar o arquivo $nameArchive: ${ex.message}")
+    }
 }
