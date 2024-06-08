4127ce9f2c53aa49d18a98fbfc6fbcfe9b328261
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-07 22:47:25 -0300

    Exercicios 11 a 12 completos.

--- kotlin/exerciciosBasicos/src/EstruturaSequencial/Exercicio11.kt
+++ kotlin/exerciciosBasicos/src/EstruturaSequencial/Exercicio11.kt
@@ -1,8 +1,33 @@
 package EstruturaSequencial
+
+import java.util.Scanner
+
 // Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
 //
 //    o produto do dobro do primeiro com metade do segundo .
 //    a soma do triplo do primeiro com o terceiro.
 //    o terceiro elevado ao cubo.
 class Exercicio11 {
+}
+
+fun main() {
+    val sn: Scanner = Scanner(System.`in`)
+    print("Digite o primeiro número inteiro: ")
+    val intOne: Int = sn.nextInt()
+    print("Digite o segundo número inteiro: ")
+    val intTwo: Int = sn.nextInt()
+    print("Digite o primeiro número real: ")
+    val doubleThree: Double = sn.nextDouble()
+
+    println(intOne * (intTwo / 2))
+    println(intOne * 3 + doubleThree)
+    println(elevadoAoCubo(doubleThree))
+    sn.close()
+
+}
+
+fun elevadoAoCubo(number: Double): Double{
+    var counter: Double = number
+    for (i in 2..3) counter *= number
+    return counter
 }
--- kotlin/exerciciosBasicos/src/EstruturaSequencial/Exercicio12.kt
+++ kotlin/exerciciosBasicos/src/EstruturaSequencial/Exercicio12.kt
@@ -1,5 +1,17 @@
 package EstruturaSequencial
+
+import java.util.Scanner
+
 // Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que
 // calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 class Exercicio12 {
+}
+
+fun main() {
+    val sn: Scanner = Scanner(System.`in`)
+    println("Digite sua altura: ")
+    val altura: Double = sn.nextDouble()
+    val pesoIdeal = (72.7 * altura) - 58
+    println("Seu peso ideal é: %.2f".format(pesoIdeal))
+    sn.close()
 }
