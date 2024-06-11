73c4dd7f9e201a213d258c6406bc6dd4756e4a85
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-11 10:10:57 -0300

    Finalizando os exercicios 05 a 07.

--- kotlin/exerciciosBasicos/src/EstruturaDeDecisao/Exercicio05.kt
+++ kotlin/exerciciosBasicos/src/EstruturaDeDecisao/Exercicio05.kt
@@ -3,3 +3,19 @@
 //    A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 //    A mensagem "Reprovado", se a média for menor do que sete;
 //    A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+fun main() {
+    print("Digite a primeira nota: ")
+    val noteOne: Double? = readLine()?.toDoubleOrNull()
+    print("Digite a segunda nota: ")
+    val noteTwo: Double? = readLine()?.toDoubleOrNull()
+    if (noteTwo != null && noteOne != null){
+        val media = (noteOne + noteTwo) / 2
+
+        if (media == 10.0) println("Aprovado com Distinção")
+        else if (media >= 7) println("Aprovado")
+        else println("Reprovado")
+    } else {
+        println("Digite valor valido. Exemplo: 7, 8, 8,5.")
+    }
+}
--- kotlin/exerciciosBasicos/src/EstruturaDeDecisao/Exercicio06.kt
+++ kotlin/exerciciosBasicos/src/EstruturaDeDecisao/Exercicio06.kt
@@ -1,1 +1,16 @@
 // Faça um Programa que leia três números e mostre o maior deles.
+
+fun main() {
+    print("Digite um número: ")
+    val one: Int? = readLine()?.toIntOrNull()
+    print("Digite um número: ")
+    val two: Int? = readLine()?.toIntOrNull()
+    print("Digite um número: ")
+    val three: Int? = readLine()?.toIntOrNull()
+
+    if (one != null && two != null && three != null){
+        if (one > two && one > three) println(one)
+        else if (two > one && two > three) println(two)
+        else println(three)
+    } else println("Digite valor valido.")
+}
